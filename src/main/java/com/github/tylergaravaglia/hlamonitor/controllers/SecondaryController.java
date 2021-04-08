b4e8e2af56bc977406811e571a1c1dbfbf4b251f
Tyler Garavaglia <tylergaravaglia@gmail.com>
2021-04-08 15:34:09 -0400

    Moves controllers into dedicated package

--- src/main/java/com/github/tylergaravaglia/hlamonitor/controllers/SecondaryController.java
+++ src/main/java/com/github/tylergaravaglia/hlamonitor/controllers/SecondaryController.java
@@ -1,6 +1,8 @@
-package com.github.tylergaravaglia.hlamonitor;
+package com.github.tylergaravaglia.hlamonitor.controllers;
 
 import java.io.IOException;
+
+import com.github.tylergaravaglia.hlamonitor.App;
 import javafx.fxml.FXML;
 
 public class SecondaryController {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module com.github.tylergaravaglia.hlamonitor {
 
     opens com.github.tylergaravaglia.hlamonitor to javafx.fxml;
     exports com.github.tylergaravaglia.hlamonitor;
+    exports com.github.tylergaravaglia.hlamonitor.controllers;
+    opens com.github.tylergaravaglia.hlamonitor.controllers to javafx.fxml;
 }
