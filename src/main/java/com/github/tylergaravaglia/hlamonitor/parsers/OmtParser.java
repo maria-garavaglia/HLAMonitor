348e2dbccfe554f2b617bd74d8f1af5ba058af25
Tyler Garavaglia <tylergaravaglia@gmail.com>
2021-04-08 16:57:50 -0400

    Starting OMT file parsing

---
+++ src/main/java/com/github/tylergaravaglia/hlamonitor/parsers/OmtParser.java
@@ -0,0 +1,6 @@
+package com.github.tylergaravaglia.hlamonitor.parsers;
+
+public interface OmtParser
+{
+    void parse(String filename);
+}
---
+++ src/main/java/com/github/tylergaravaglia/hlamonitor/parsers/RprFomParser.java
@@ -0,0 +1,11 @@
+package com.github.tylergaravaglia.hlamonitor.parsers;
+
+public class RprFomParser implements OmtParser
+{
+
+    @Override
+    public void parse(String filename)
+    {
+
+    }
+}
